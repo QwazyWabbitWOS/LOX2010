c63cbace75892b0592fe9f17a495d91564f3f8e7
QwazyWabbit <gandalf.2004@gmail.com>
2018-12-01 11:50:07 -0800

    Delete unused oldgoal in UpdateChaseCam

--- g_chase.c
+++ g_chase.c
@@ -8,7 +8,7 @@ void UpdateChaseCam(edict_t *ent)
 	vec3_t forward, right;
 	trace_t trace;
 	int i;
-	vec3_t oldgoal;
+	//vec3_t oldgoal;
 	vec3_t angles;
 
 	// is our chase target gone?
@@ -28,7 +28,7 @@ void UpdateChaseCam(edict_t *ent)
 	targ = ent->client->chase_target;
 
 	VectorCopy(targ->s.origin, ownerv);
-	VectorCopy(ent->s.origin, oldgoal);
+	//VectorCopy(ent->s.origin, oldgoal);
 
 	ownerv[2] += targ->viewheight;
 
