c765c88fa89f3f4a5dd0f7d4d4e6870b1b095698
QwazyWabbit <gandalf.2004@gmail.com>
2020-02-27 10:46:41 -0800

    Minor reformat of ReadEntFile and LoadEntFile functions.

--- g_ent.c
+++ g_ent.c
@@ -1,55 +1,54 @@
 #include "g_local.h" 
 
-static char *ReadEntFile(char *filename) 
-{ 
-	
-	FILE      *fp; 
-	char      *filestring = NULL; 
-	long int   i = 0; 
-	int         ch; 
-	
-	for (;;) 
-	{ 
-		fp = fopen(filename, "r"); 
-		if (!fp) break; 
-		
-		for (i=0; (ch = fgetc(fp)) != EOF; i++); 
-		
-		filestring = gi.TagMalloc(i+1, TAG_LEVEL); 
-		if (!filestring) break; 
-		
-		fseek(fp, 0, SEEK_SET); 
-		for (i=0; (ch = fgetc(fp)) != EOF; i++) 
-			filestring[i] = (char) ch; 
-		filestring[i] = '\0'; 
-		break; 
-	} 
-	
-	if (fp) fclose(fp); 
-	return(filestring); 
-} 
+static char* ReadEntFile(char* filename)
+{
 
-char *LoadEntFile(char *mapname, char *entities) 
-{ 
-	char   entfilename[MAX_QPATH] = ""; 
-	char   *newentities; 
-	int      i; 
-    
-	sprintf(entfilename, "entfiles/%s.ent", mapname); 
+	FILE*	fp;
+	char*	filestring = NULL;
+	long int	i = 0;
+	int		ch;
+
+	while (1)
+	{
+		fp = fopen(filename, "r");
+		if (!fp) break;
+
+		for (i = 0; (ch = fgetc(fp)) != EOF; i++);
+
+		filestring = gi.TagMalloc(i + 1, TAG_LEVEL);
+		if (!filestring) break;
+
+		fseek(fp, 0, SEEK_SET);
+		for (i = 0; (ch = fgetc(fp)) != EOF; i++)
+			filestring[i] = (char)ch;
+		filestring[i] = '\0';
+		break;
+	}
+
+	if (fp) fclose(fp);
+	return(filestring);
+}
+
+char* LoadEntFile(char* mapname, char* entities)
+{
+	char	entfilename[MAX_QPATH] = "";
+	char*	newentities;
+
+	Com_sprintf(entfilename, sizeof entfilename, "entfiles/%s.ent", mapname);
 	// convert string to all lowercase 
-	for (i = 0; entfilename[i]; i++) 
-		entfilename[i] = (char) tolower(entfilename[i]); 
-	
-	newentities = ReadEntFile(entfilename); 
-	
-	if (newentities) 
-	{   //leave these gi.dprints active, they show up in the server console 
-		gi.dprintf("LOX: Entity file %s.ent found\n", mapname); 
+	for (int i = 0; entfilename[i]; i++)
+		entfilename[i] = (char)tolower(entfilename[i]);
+
+	newentities = ReadEntFile(entfilename);
+
+	if (newentities)
+	{
+		gi.dprintf("LOX: Entity file %s.ent found\n", mapname);
 		return(newentities);   // reassign the ents 
-	} 
-	else 
-	{ 
-		gi.dprintf("LOX: No entity file for %s.bsp\n", mapname); 
-		return(entities); 
-	} 
-} 
+	}
+	else
+	{
+		gi.dprintf("LOX: No entity file for %s.bsp\n", mapname);
+		return(entities);
+	}
+}
